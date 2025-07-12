e9e99650f140b8ee1d0f0821f172e69c0951c1cf
SarthakDudhe <harryop29@gmail.com>
2025-07-12 11:27:33 +0530

    one change

--- server/server.js
+++ server/server.js
@@ -43,7 +43,7 @@ Sentry.setupExpressErrorHandler(app);
 
 
 if (process.env.NODE_ENV !=="production") {
-  const PORT=process.env.PORT || 5000;
+ 
 app.listen(PORT,()=>console.log("Server is running on PORT : ",PORT));  
 }
 
